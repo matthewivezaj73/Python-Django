ec18dd1b6f20149bf2688a5deefd50465b62f147
Matthew Ivezaj <matthew.ivezaj73@gmail.com>
2022-09-22 20:52:36 -0400

    Assigning the name of the passed obj to a variable.

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello, world!")
+    name = request.Get.get("name") or "world"
