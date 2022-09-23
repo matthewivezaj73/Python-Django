7b0784df4b1df60eaf7d69c2efa47796e6ff6469
Matthew Ivezaj <matthew.ivezaj73@gmail.com>
2022-09-23 06:09:41 -0400

    Commit

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def index(request):
     #added a request to get the name of the user, or 
     # add world and assign the result to name.
-    name = request.Get.get("name") #or "world"
+    name = request.GET.get("name") #or "world"
     return HttpResponse(f"Hello, {name}!")
