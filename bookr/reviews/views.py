7a7001bf6c88356e865d148cf752e515c6a1d182
Matthew Ivezaj <matthew.ivezaj73@gmail.com>
2022-09-22 21:02:10 -0400

    added a request to get the name of the user, or add world and assign the result to name.

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
-
 def index(request):
     name = request.Get.get("name") or "world"
-    return HttpResponse("Hello, {}!".format(name))
+    return HttpResponse("Hello, world!")
